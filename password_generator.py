f625567a035fd99da0692a9afb6bcc0df7ced9c7
Sharva-k <kalesharva@gmail.com>
2025-12-22 17:22:41 +0530

    Python

--- password_generator.py
+++ password_generator.py
@@ -1,7 +1,23 @@
 import string
+import random
 letters = list(string.ascii_lowercase)
 digits = list(string.digits)
 symbols = list(string.punctuation)
-print(symbols)
-print(digits)
-print(letters)
+
+
+# easy generator
+
+password = ""
+
+in_letters = int(input("Enter number of letters you want in your password: "))
+in_symbols = int(input("Enter number of symbols you want in your password: "))
+in_numbers = int(input("Enter number of numbers you want in your password: "))
+
+for i in range(1,in_letters+1):
+    password+= random.choice(letters)
+for i in range(1, in_symbols):
+    password+= random.choice(symbols)
+for i in range(1,in_numbers):
+    password+= random.choice(digits)
+
+print(password)
