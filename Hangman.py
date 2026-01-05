611bfcba5ea8fb10c7abc4729ce212bddadad676
Sharva-k <kalesharva@gmail.com>
2026-01-05 21:51:28 +0530

    Python

--- Hangman.py
+++ Hangman.py
@@ -1,4 +1,60 @@
 import random
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 word_list = ["Cake" , "Guitar" , "Pen"]
 chosen_word = random.choice(word_list)
 print(chosen_word)
@@ -8,14 +64,25 @@ for i in range(len(chosen_word)):
     placeholder+="_ "
 print(placeholder)
 
-guess = input("\nGuess a letter: ").lower()
+game_over = False
 
-display = ""
+correct_letters = []
 
-for letter in chosen_word:
-    if letter == guess:
-        display+=letter
-    else:
-        display+=" _ "
-print(display)
-     
+while not game_over:
+
+    guess = input("\nGuess a letter: ").lower()
+
+    display = ""
+
+    for letter in chosen_word:
+        if letter == guess:
+            display+=letter
+            correct_letters.append(letter)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display+=" _ "
+    print(display)
+    if "_" not in display:
+        game_over = True
+        print("You win!")
