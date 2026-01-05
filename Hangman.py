86d49072fc2d51fee99658a8e06eabd65434d07d
Sharva-k <kalesharva@gmail.com>
2026-01-05 21:35:46 +0530

    Python

--- Hangman.py
+++ Hangman.py
@@ -4,4 +4,10 @@ chosen_word = random.choice(word_list)
 print(chosen_word)
 
 guess = input("Guess a letter: ").lower()
-print(guess)
+
+for letter in chosen_word:
+    if letter == guess:
+        print("Right!")
+    else:
+        print("Wrong!")
+     
