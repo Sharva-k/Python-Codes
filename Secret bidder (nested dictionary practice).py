84c9c5c4fa174ae5894d7e2ad5c04211ae50ccbf
Sharva-k <kalesharva@gmail.com>
2026-01-07 21:42:12 +0530

    Python

--- Secret bidder (nested dictionary practice).py
+++ Secret bidder (nested dictionary practice).py
@@ -5,3 +5,9 @@ while continue_bidding:
     price = int(input("What is you bid?: "))
     bids[name] = price
     should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
+
+    if should_continue == "no":
+        continue_bidding = False
+        find_highest_bidder(bids)
+    elif should_continue == "yes":
+        print("\n" * 20)
