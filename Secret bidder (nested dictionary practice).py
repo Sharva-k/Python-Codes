bf79ad0ebdf36e7d4e045ba8cd876e73f1132f79
Sharva-k <kalesharva@gmail.com>
2026-01-07 21:44:45 +0530

    Python

--- Secret bidder (nested dictionary practice).py
+++ Secret bidder (nested dictionary practice).py
@@ -1,3 +1,14 @@
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner =""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount>highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+
 bids={}
 continue_bidding =True
 while continue_bidding:
@@ -10,4 +21,4 @@ while continue_bidding:
         continue_bidding = False
         find_highest_bidder(bids)
     elif should_continue == "yes":
-        print("\n" * 20)
+        
