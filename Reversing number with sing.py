3dcc22bb099377a48fd39b7d8357e23dd4722fe6
Sharva-k <kalesharva@gmail.com>
2026-01-15 10:00:54 +0530

    Python

---
+++ Reversing number with sing.py
@@ -0,0 +1,21 @@
+def reverse(x):
+
+    if x<0:
+        sign = -1
+        x*= -1
+    elif x>0:
+        sign = +1
+    else:
+        return 0
+
+
+
+    answer = 0
+    while x:
+        if answer<= (2^31 - (x%10))/10:
+            answer = answer*10 + x%10
+            x//=10
+        else:
+            return 0
+    return answer*sign
+print(reverse(-123))
