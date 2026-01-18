6c39f2db71878c719941e25708ed5bf56a7604ea
Sharva-k <kalesharva@gmail.com>
2026-01-18 12:43:28 +0530

    Python

--- Spiral Matrix.py
+++ Spiral Matrix.py
@@ -1,27 +1,27 @@
-matrix = [[1,2,3],[4,5,6],[7,8,9]]
-output =[]
-top = 0
-bottom =len(matrix)-1
-right = len(matrix[0])-1
-left = 0
+def spiral(matrix):
+    matrix = [[1,2,3],[4,5,6],[7,8,9]]
+    output =[]
+    top = 0
+    bottom =len(matrix)-1
+    right = len(matrix[0])-1
+    left = 0
 
-while top<=bottom and left<= right: #top boundary
-    for i in range(left, right+1):
-        output.append(matrix[top][i])
+    while top<=bottom and left<= right: #top boundary
+        for i in range(left, right+1):
+            output.append(matrix[top][i])
 
-    top+=1
-    for i in range(top,bottom+1):
-        output.append(matrix[i][right])
+        top+=1
+        for i in range(top,bottom+1):
+            output.append(matrix[i][right])
 
-    right-=1
-    if left<=right:
-        for i in range(right,left-1,-1):
-            output.append(matrix[bottom][i])
-    bottom-=1
+        right-=1
+        if left<=right:
+            for i in range(right,left-1,-1):
+                output.append(matrix[bottom][i])
+        bottom-=1
 
-    if top<=bottom:
-        for i in range(bottom,top-1,-1):
-            output.append(matrix[i][left])
-    left+=1
+        if top<=bottom:
+            for i in range(bottom,top-1,-1):
+                output.append(matrix[i][left])
+        left+=1
 
-print(output)
