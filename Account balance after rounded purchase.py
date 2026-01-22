d1a61290547bc9401b07c4afd0851b0fe7573139
Sharva-k <kalesharva@gmail.com>
2026-01-22 10:30:39 +0530

    Python

--- Account balance after rounded purchase.py
+++ Account balance after rounded purchase.py
@@ -1,8 +1,11 @@
-
 def bal(purchaseAmount):
     balance = 100
-    while purchaseAmount%10 != 0 or purchaseAmount == 0:
-        purchaseAmount+=1
+    if purchaseAmount%10 != 0 or purchaseAmount != 0:
+        temp = purchaseAmount%10
+        if temp>5:
+            purchaseAmount-= temp
+        else:
+            purchaseAmount+=(10-temp)
     balance-=purchaseAmount
     return balance
 print(bal(4))
