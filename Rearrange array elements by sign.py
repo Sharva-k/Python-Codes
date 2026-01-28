9e13bc15459d11761930d633567ef1739fb3dd73
Sharva-k <kalesharva@gmail.com>
2026-01-28 21:05:45 +0530

    Python

---
+++ Rearrange array elements by sign.py
@@ -0,0 +1,12 @@
+nums = [3,1,-2,-5,2,-4]
+
+pos , neg = 0,1
+res = [0] * len(nums)
+for i in nums:
+    if i > 0:
+        res[pos] = i
+        pos+=2
+    elif i<0:
+        res[neg] = i
+        neg+=2
+
